24d712b2c77eecc8d7cab470d6336e62c7050a3a
VisEntities <danaheinz1000@gmail.com>
2023-07-06 20:19:33 +0200

    Increment patch version

--- AutomatedStashTraps.cs
+++ AutomatedStashTraps.cs
@@ -20,7 +20,7 @@ using Random = UnityEngine.Random;
 
 namespace Oxide.Plugins
 {
-    [Info("Automated Stash Traps", "Dana", "1.5.2")]
+    [Info("Automated Stash Traps", "Dana", "1.5.3")]
     [Description("Spawns fully automated stash traps across the map to catch ESP cheaters.")]
     public class AutomatedStashTraps : RustPlugin
     {
